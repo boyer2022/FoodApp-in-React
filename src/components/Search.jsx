40281a1751f73a5a8ac7ae7ddf45b8fc7ee7b650
Matthew Boyer <m.boyer2016@outlook.com>
2025-01-16 16:43:54 -0600

    end of vid 48

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,27 @@
 // Creating the Search field
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Search() {
   // Utilizing the useState function
   // useState default is set to pizza for API callback
   const [query, setQuery] = useState("pizza");
+  //   useEffect hook
+  // Syntax of the useEffect hook
+  // Pass in the callback function ()=>{}, then the dependency array []
+  // Whenever value changes-reinitialize the useEffect hook by passing the query into the dependency array[]
+
+  // Simplest example:
+  // useEffect(()=>{
+  // function demo() {
+  // console.log("Demo Function Executed")
+  // }
+  // demo()
+  // }, [query]);
+
+  // Whenever value changes-reinitialize the useEffect hook by passing the query into the dependency array[]
+  useEffect(() => {}, []);
+  useEffect(() => {}, []);
   return (
     <div>
       <input
